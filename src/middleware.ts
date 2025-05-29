6bd8c30641eb95020f00c64b8308950bcd61fb52
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-29 17:30:52 +0300

    Add a middleware to validate the user auth cookie

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import {
+  signoutAndRedirect,
+  getResWithXHeaders,
+  getSignedInUser,
+} from './lib/auth';
+
+const authUrls = ['/signin', '/signup'];
+
+export default async function middleware(req: NextRequest) {
+  const user = await getSignedInUser();
+
+  const isAuthUrl = authUrls.includes(req.nextUrl.pathname);
+
+  if (!user && !isAuthUrl) {
+    return signoutAndRedirect(req, '/signin');
+  }
+
+  if (user && isAuthUrl) {
+    return NextResponse.redirect(new URL('/', req.nextUrl));
+  }
+
+  return getResWithXHeaders(req, NextResponse.next());
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
