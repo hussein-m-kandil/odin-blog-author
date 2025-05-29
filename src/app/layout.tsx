28f01d3f04f4604a42cee3b7ef8f7e5d38f84179
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-29 17:49:55 +0300

    Simplify the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import './globals.css';
 
-import { Geist, Geist_Mono } from 'next/font/google';
-import { H1 } from '@/components/typography/h1';
 import { Metadata } from 'next';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: { template: '%s | Odin Blog Author', default: 'Odin Blog Author' },
@@ -21,20 +9,14 @@ export const metadata: Metadata = {
   icons: '/top.svg',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header>
-          <H1>Odin Blog Author</H1>
-        </header>
-        <main>{children}</main>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
