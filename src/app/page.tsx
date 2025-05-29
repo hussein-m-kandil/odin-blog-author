8bad24e500633813bdff6de8934840c3530c87b1
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-29 17:55:46 +0300

    Verify user auth by fetching its data in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Home page...</div>;
+import { getSignedInUser } from '@/lib/auth';
+import { Navbar } from '@/components/navbar';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+  const user = await getSignedInUser();
+
+  if (!user) {
+    return redirect('/signin');
+  }
+
+  return (
+    <>
+      <header>
+        <Navbar user={user} />
+      </header>
+      <main>
+        <div>Home page...</div>
+      </main>
+    </>
+  );
 }
