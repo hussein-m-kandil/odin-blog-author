8f0ff33207eb21bda9bdf59a5d62d56d527c8a20
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-30 21:46:58 +0300

    Mock `ResizeObserver` in global test environment

--- src/vitest-setup.ts
+++ src/vitest-setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/vitest';
+import { beforeAll } from 'vitest';
+
+beforeAll(() => {
+  global.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+});
