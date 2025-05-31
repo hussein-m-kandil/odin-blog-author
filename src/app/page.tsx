847a2f3f31ea5127163d7e8f402f8887366288a8
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-05-31 16:18:39 +0300

    Fetch and display user posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Post } from '@/types';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { getSignedInUser } from '@/lib/auth';
 import { Navbar } from '@/components/navbar';
-import { redirect } from 'next/navigation';
+import { PostCard } from '@/components/post-card';
 
 export default async function Home() {
   const user = await getSignedInUser();
@@ -9,13 +12,27 @@ export default async function Home() {
     return redirect('/signin');
   }
 
+  const userPosts: Post[] = [];
+  const headerStore = await headers();
+  const currentUrl = headerStore.get('referer') || headerStore.get('x-url');
+  if (currentUrl) {
+    const userPostsPath = `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/${user.id}/posts`;
+    const userPostsUrl = new URL(userPostsPath, new URL(currentUrl).origin);
+    const userPostsRes = await fetch(userPostsUrl);
+    userPosts.push(...(await userPostsRes.json()));
+  }
+
   return (
     <>
       <header>
         <Navbar user={user} />
       </header>
       <main>
-        <div>Home page...</div>
+        <div className='max-w-md mx-auto mt-10 overflow-hidden space-y-12'>
+          {userPosts.map((post) => (
+            <PostCard key={post.id} post={post} isMutable={true} />
+          ))}
+        </div>
       </main>
     </>
   );
