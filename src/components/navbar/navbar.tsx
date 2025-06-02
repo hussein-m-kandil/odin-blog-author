49a74774658350516920f0e18866196b8b738326
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-02 16:35:32 +0300

    Add trigger for create-post-form in the `Navbar`

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,9 +5,15 @@ import { User } from '@/types';
 import { H1 } from '../typography/h1';
 import { Button } from '../ui/button';
 import { usePathname } from 'next/navigation';
+import { PostFormDialog } from '../post-form-dialog';
 
 export function Navbar({ user = null }: { user?: User | null }) {
   const pathname = usePathname();
+
+  const btnCommonProps: { variant: 'outline'; className: string } = {
+    variant: 'outline',
+    className: 'font-bold',
+  };
 
   return (
     <nav className='flex items-center justify-between p-4 shadow-sm shadow-secondary'>
@@ -15,13 +21,18 @@ export function Navbar({ user = null }: { user?: User | null }) {
         <Link href='/'>Odin Blog Author</Link>
       </H1>
       {user ? (
-        <form
-          action={`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/signout`}
-          method='post'>
-          <Button type='submit'>Sign out</Button>
-        </form>
+        <div className='flex items-center gap-4'>
+          <PostFormDialog />
+          <form
+            action={`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/signout`}
+            method='post'>
+            <Button type='submit' {...btnCommonProps}>
+              Sign out
+            </Button>
+          </form>
+        </div>
       ) : (
-        <Button type='button' asChild>
+        <Button type='button' {...btnCommonProps} asChild>
           {pathname === '/signin' ? (
             <Link href='/signup'>Sign up</Link>
           ) : (
