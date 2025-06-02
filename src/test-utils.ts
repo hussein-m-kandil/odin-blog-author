75f1d1136d0ac05d609d545c4ed509e80be1b3a3
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-02 16:16:41 +0300

    Add some post data in global test-utils module

---
+++ src/test-utils.ts
@@ -0,0 +1,45 @@
+import { User, Post } from '@/types';
+
+export const dates = {
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+};
+
+export const author: User = {
+  ...dates,
+  bio: null,
+  isAdmin: false,
+  id: 'blahblah123user',
+  username: 'nowhere_man',
+  fullname: 'Nowhere-Man',
+};
+
+export const post: Post = {
+  author,
+  ...dates,
+  published: true,
+  title: 'Test Post',
+  id: 'blahblah123post',
+  authorId: author.id,
+  content: 'Just for testing...',
+  categories: [{ name: 'Software' }],
+  comments: [
+    {
+      author,
+      ...dates,
+      authorId: author.id,
+      content: 'Test comment',
+      id: 'blahblah123comment',
+      postId: 'blahblah123post',
+    },
+  ],
+  votes: [
+    {
+      user: author,
+      isUpvote: true,
+      userId: author.id,
+      id: 'blahblah123vote',
+      postId: 'blahblah123post',
+    },
+  ],
+};
