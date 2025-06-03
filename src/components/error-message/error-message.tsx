344fbaa31914094086b6a61cefbbd01538130441
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-03 13:32:13 +0300

    Add `ErrorMessage` component

---
+++ src/components/error-message/error-message.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+import { P } from '@/components/typography/p';
+
+export function ErrorMessage({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<'p'>) {
+  return (
+    <P {...props} className={cn('text-destructive text-center', className)}>
+      {children}
+    </P>
+  );
+}
+
+export default ErrorMessage;
