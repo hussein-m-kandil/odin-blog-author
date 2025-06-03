29958a92e988926bc8bca395fa88e4efee0ae71d
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-03 09:55:25 +0300

    Exclude some image suffixes from auth verification

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,5 +25,7 @@ export default async function middleware(req: NextRequest) {
 
 // Routes Middleware should not run on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|[^/]*\\.(?:.ico|svg|png|jpg|jpeg)$).*)',
+  ],
 };
