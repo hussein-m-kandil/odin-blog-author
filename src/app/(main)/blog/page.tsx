cebd5fcc5933cc8f93385358fee7b1db6deed90f
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-04 16:06:23 +0300

    Display a message on an empty blog page

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage } from '@/components/error-message';
 import { authedFetch, getUserId } from '@/lib/auth';
 import { PostCard } from '@/components/post-card';
+import { P } from '@/components/typography/p';
 import { Post } from '@/types';
 
 export default async function Blog() {
@@ -14,13 +15,15 @@ export default async function Blog() {
 
   const userPosts: Post[] = await apiRes.json();
 
+  if (userPosts.length < 1) {
+    return <P className='text-center'>There are no posts yet</P>;
+  }
+
   return (
-    <main>
-      <div className='max-w-md mx-auto mt-10 overflow-hidden space-y-12'>
-        {userPosts.map((post) => (
-          <PostCard key={post.id} post={post} isMutable={true} />
-        ))}
-      </div>
+    <main className='max-w-md mx-auto mt-10 px-3 space-y-12 overflow-hidden'>
+      {userPosts.map((post) => (
+        <PostCard key={post.id} post={post} isMutable={true} />
+      ))}
     </main>
   );
 }
