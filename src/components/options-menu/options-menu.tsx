df0b4437cd987070372734c8d12e852c0fc02869
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-12 16:31:49 +0300

    Extract `OptionsMenu` component from `PostCard`

---
+++ src/components/options-menu/options-menu.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from '@/components/ui/dropdown-menu';
+import { EllipsisVertical } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+export function OptionsMenu({
+  triggerLabel,
+  triggerCN,
+  menuLabel,
+  menuItems,
+  menuCN,
+}: {
+  menuItems?: React.ReactNode | React.ReactNode[];
+  triggerLabel?: string;
+  triggerCN?: string;
+  menuLabel?: string;
+  menuCN?: string;
+}) {
+  const items = Array.isArray(menuItems) ? menuItems : [menuItems];
+
+  if (items.length < 1 || items.every((i) => !i)) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        aria-label={triggerLabel || 'Open options menu'}
+        className={cn(
+          'focus-visible:outline-0 text-muted-foreground hover:text-foreground focus-visible:text-foreground',
+          triggerCN
+        )}>
+        <EllipsisVertical />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className={menuCN}
+        aria-label={menuLabel || 'Options menu'}>
+        {items.map(
+          (i, index) =>
+            (i || i === 0) && (
+              <DropdownMenuItem key={index} asChild={typeof i === 'object'}>
+                {i}
+              </DropdownMenuItem>
+            )
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default OptionsMenu;
