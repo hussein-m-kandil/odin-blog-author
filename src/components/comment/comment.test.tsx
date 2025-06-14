a65440c73eb80743d15426606d61491e8117b3c8
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-14 13:18:41 +0300

    Add `Comment` component

---
+++ src/components/comment/comment.test.tsx
@@ -0,0 +1,79 @@
+import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { mockDialogContext, post } from '@/test-utils';
+import { describe, expect, it } from 'vitest';
+import { Comment } from './comment';
+
+const comment = post.comments[0];
+const authorId = post.authorId;
+
+const props = { post, comment };
+
+const optsRegex = /open.*options/i;
+
+mockDialogContext();
+
+describe('<Comments />', () => {
+  it('should render the post comments', async () => {
+    render(<Comment {...props} />);
+    expect(screen.getByText(comment.content)).toBeInTheDocument();
+  });
+
+  it('should have the given class', async () => {
+    const htmlClass = 'blah';
+    render(<Comment {...props} className={htmlClass} />);
+    expect(screen.getByText(comment.content)).toHaveClass(htmlClass);
+  });
+
+  it('should not render comment options menu if not given the current user id', async () => {
+    render(<Comment {...props} />);
+    expect(screen.queryByRole('button', { name: optsRegex })).toBeNull();
+  });
+
+  it('should not render comment options menu for a user who is not for post author, nor comment author', async () => {
+    render(<Comment {...props} currentUserId={'foreign-user'} />);
+    expect(screen.queryByRole('button', { name: optsRegex })).toBeNull();
+  });
+
+  it('should render (collapsed) comment options menu if given current user id', async () => {
+    render(<Comment {...props} currentUserId={authorId} />);
+    expect(screen.getByRole('button', { name: optsRegex })).toBeInTheDocument();
+    expect(screen.queryByRole('menuitem', { name: /delete/i })).toBeNull();
+    expect(screen.queryByRole('menuitem', { name: /update/i })).toBeNull();
+  });
+
+  it('should options menu have delete & update options for a user who is comment author', async () => {
+    const user = userEvent.setup();
+    render(<Comment {...props} currentUserId={authorId} />);
+    await user.click(screen.getByRole('button', { name: optsRegex }));
+    expect(screen.getByRole('menuitem', { name: /update/i })).toBeVisible();
+    expect(screen.getByRole('menuitem', { name: /delete/i })).toBeVisible();
+  });
+
+  it('should options menu have only a delete option for a user who is post author, but not comment author', async () => {
+    const user = userEvent.setup();
+    const otherPostAuthorId = 'other-author';
+    const otherPost = {
+      ...post,
+      authorId: otherPostAuthorId,
+      author: { ...post.author, id: otherPostAuthorId },
+    };
+    render(
+      <Comment
+        {...{ ...props, post: otherPost }}
+        currentUserId={otherPostAuthorId}
+      />
+    );
+    await user.click(screen.getByRole('button', { name: optsRegex }));
+    expect(screen.getByRole('menuitem', { name: /delete/i })).toBeVisible();
+    expect(screen.queryByRole('menuitem', { name: /update/i })).toBeNull();
+  });
+
+  it('should show update comment form on update, and hide it after update', async () => {
+    const user = userEvent.setup();
+    render(<Comment {...props} currentUserId={authorId} />);
+    await user.click(screen.getByRole('button', { name: optsRegex }));
+    await user.click(screen.getByRole('menuitem', { name: /update/i }));
+    expect(screen.getByRole('form', { name: /update/i })).toBeInTheDocument();
+  });
+});
---
+++ src/components/comment/comment.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import React from 'react';
+import { toast } from 'sonner';
+import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
+import { useDialog } from '@/contexts/dialog-context';
+import { DeleteForm } from '@/components/delete-form';
+import { OptionsMenu } from '@/components/options-menu';
+import { CommentForm } from '@/components/comment-form';
+import { Comment as CommentType, ID, Post } from '@/types';
+
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export function Comment({
+  currentUserId,
+  className,
+  comment,
+  post,
+  ...props
+}: React.ComponentProps<'div'> & {
+  currentUserId?: ID | null;
+  comment: CommentType;
+  post: Post;
+}) {
+  const [updating, setUpdating] = React.useState(false);
+  const router = useRouter();
+
+  const { showDialog, hideDialog } = useDialog();
+
+  const enterDelete = () => {
+    showDialog({
+      title: 'Delete Comment',
+      description: 'Confirm deleting this comment',
+      body: (
+        <DeleteForm
+          method='dialog'
+          subject={comment.content}
+          onCancel={hideDialog}
+          onSuccess={() => {
+            hideDialog();
+            router.replace(`/blog/${comment.postId}`, { scroll: false });
+            toast.success('Comment Deleted', {
+              description: 'You have deleted a comment successfully',
+            });
+          }}
+          delReqFn={() =>
+            fetch(
+              `${apiBaseUrl}/posts/${comment.postId}/comments/${comment.id}`,
+              { method: 'DELETE' }
+            )
+          }
+        />
+      ),
+    });
+  };
+
+  const exitUpdate = () => setUpdating(false);
+
+  return updating ? (
+    <CommentForm
+      onSuccess={exitUpdate}
+      onCancel={exitUpdate}
+      comment={comment}
+    />
+  ) : (
+    <div
+      {...props}
+      className={cn(
+        'bg-secondary/50 py-6 px-8 rounded-md border border-input',
+        'flex justify-between items-center',
+        className
+      )}>
+      {comment.content}
+      {(currentUserId === post.authorId ||
+        currentUserId === comment.authorId) && (
+        <OptionsMenu
+          menuLabel='Comment options menu'
+          menuCN='*:w-full *:text-start'
+          triggerCN='w-4 overflow-hidden'
+          triggerLabel='Open comment options menu'
+          menuItems={
+            currentUserId === comment.authorId ? (
+              [
+                <button
+                  type='button'
+                  key={`update-${comment.id}`}
+                  onClick={() => setUpdating(true)}>
+                  Update
+                </button>,
+                <button
+                  type='button'
+                  onClick={enterDelete}
+                  key={`delete-${comment.id}`}
+                  className='text-destructive!'>
+                  Delete
+                </button>,
+              ]
+            ) : (
+              <button type='button'>Delete</button>
+            )
+          }
+        />
+      )}
+    </div>
+  );
+}
+
+export default Comment;
