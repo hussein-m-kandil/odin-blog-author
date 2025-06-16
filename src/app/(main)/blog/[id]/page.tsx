c286a40ea08cb3edb14bfa0d89a2d5e08290adcd
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-16 11:29:47 +0300

    Add section for comments to blog detail page

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { PostFormDialog } from '@/components/post-form-dialog';
+import { BlogComments } from '@/components/blog-comments';
 import { ErrorMessage } from '@/components/error-message';
+import { Comment as CommentType, Post } from '@/types';
 import { Muted } from '@/components/typography/muted';
 import { Separator } from '@/components/ui/separator';
 import { Categories } from '@/components/categories';
 import { authedFetch, getUserId } from '@/lib/auth';
 import { Lead } from '@/components/typography/lead';
 import { H2 } from '@/components/typography/h2';
+import { H3 } from '@/components/typography/h3';
 import { formatDate } from '@/lib/utils';
-import { Post } from '@/types';
 
 export default async function BlogPost({
   params,
@@ -18,17 +20,21 @@ export default async function BlogPost({
 
   const userId = await getUserId();
 
-  const apiRes = await authedFetch(`/posts/${id}`);
+  const [commentsRes, postRes] = await Promise.all([
+    authedFetch(`/posts/${id}/comments`),
+    authedFetch(`/posts/${id}`),
+  ]);
 
   const errMsg = (
     <ErrorMessage>Sorry, we could not get the post data</ErrorMessage>
   );
 
-  if (!apiRes.ok) return errMsg;
+  if (!postRes.ok || !commentsRes.ok) return errMsg;
 
-  const post = (await apiRes.json()) as Post;
+  const post = (await postRes.json()) as Post;
+  const comments = (await commentsRes.json()) as CommentType[];
 
-  if (!post) return errMsg;
+  if (!post || !comments) return errMsg;
 
   const commonTriggerProps = {
     className: 'w-auto align-middle cursor-pointer',
@@ -36,7 +42,7 @@ export default async function BlogPost({
   };
 
   return (
-    <main className='max-w-2xl px-3 mx-auto'>
+    <main className='max-w-2xl mx-auto'>
       {userId === post.authorId && (
         <div className='flex justify-center space-x-4 h-6 mt-4'>
           <PostFormDialog {...commonTriggerProps} />
@@ -44,7 +50,7 @@ export default async function BlogPost({
           <PostFormDialog {...commonTriggerProps} showDeleteForm={true} />
         </div>
       )}
-      <div className='mt-2 space-y-12'>
+      <div className='my-2 space-y-12'>
         <div>
           <H2 className='text-center text-2xl'>{post.title}</H2>
           <div className='flex items-center justify-between italic'>
@@ -54,6 +60,10 @@ export default async function BlogPost({
         </div>
         <Lead className='text-foreground font-light'>{post.content}</Lead>
         <Categories categories={post.categories} className='justify-end' />
+      </div>
+      <div className='my-4'>
+        <H3 className='text-center text-xl'>Comments</H3>
+        <BlogComments comments={comments} post={post} currentUserId={userId} />
       </div>
     </main>
   );
