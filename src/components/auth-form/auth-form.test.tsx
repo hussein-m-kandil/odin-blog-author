07fe28e1fd7c7b5bed7f3f313001940840749d4b
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-18 13:37:13 +0300

    Refactor `AuthForm` test

--- src/components/auth-form/auth-form.test.tsx
+++ src/components/auth-form/auth-form.test.tsx
@@ -6,9 +6,9 @@ import {
 } from './auth-form.data';
 import { render, screen, waitFor } from '@testing-library/react';
 import { afterEach, describe, expect, it, vi } from 'vitest';
+import { AuthFormProps, FormType } from './auth-form.types';
 import { userEvent } from '@testing-library/user-event';
 import { author, delay } from '@/test-utils';
-import { FormType } from './auth-form.types';
 import { AuthForm } from './auth-form';
 
 const fetchSpy = vi.spyOn(window, 'fetch').mockImplementation(() => {
@@ -19,15 +19,14 @@ const fetchSpy = vi.spyOn(window, 'fetch').mockImplementation(() => {
 
 afterEach(vi.clearAllMocks);
 
-const setup = async (formType: FormType, userUpdate = false) => {
-  const formLabelId = formType;
+const setup = async (props: AuthFormProps) => {
   const data =
-    formType === 'signin'
+    props.formType === 'signin'
       ? {
           submitterOpts: { name: /(sign ?in)/i },
           entries: Object.entries(signinFormAttrs),
         }
-      : userUpdate
+      : props.user
       ? {
           submitterOpts: { name: /(update)/i },
           entries: Object.entries(injectDefaults(updateUserFormAttrs, author)),
@@ -36,55 +35,51 @@ const setup = async (formType: FormType, userUpdate = false) => {
           submitterOpts: { name: /(sign ?up)/i },
           entries: Object.entries(signupFormAttrs),
         };
-  const props = userUpdate
-    ? { formType, formLabelId, user: author }
-    : { formType, formLabelId };
   const user = userEvent.setup();
-  const renderResult = render(<AuthForm {...props} />);
-  return { data, user, ...renderResult };
-};
-
-const assertFormAndInputExists = async (formType: FormType) => {
-  const { data } = await setup(formType);
-  const form = screen.getByRole('form');
-  expect(form).toBeInTheDocument();
-  for (const [inpName, attrs] of data.entries) {
-    const inp = screen.getByLabelText(attrs.label) as HTMLInputElement;
-    expect(inp).toBeInTheDocument();
-    expect(inp.name).toBe(inpName);
-    expect(inp.defaultValue).toBe(attrs.defaultValue);
-  }
-};
-
-const assertNotSubmitWithEmptyFields = async (formType: FormType) => {
-  const { data, user } = await setup(formType);
-  for (const entry of data.entries) {
-    const inp = screen.getByLabelText(entry[1].label) as HTMLInputElement;
-    expect(inp.ariaInvalid).toBe('false');
-  }
-  const submitter = screen.getByRole('button', data.submitterOpts);
-  await user.click(submitter);
-  for (const entry of data.entries) {
-    const inp = screen.getByLabelText(entry[1].label) as HTMLInputElement;
-    expect(inp.ariaInvalid).toBe('true');
-  }
+  return { data, user, ...render(<AuthForm {...props} />) };
 };
 
 describe(`<AuthForm />`, () => {
   for (const formType of ['signin', 'signup'] as FormType[]) {
+    const props = { formType, formLabelId: `test-${formType}-form` };
+
     describe(formType, () => {
       it('should render a form with inputs', async () => {
-        await assertFormAndInputExists(formType);
+        const { data } = await setup(props);
+        const form = screen.getByRole('form');
+        expect(form).toBeInTheDocument();
+        for (const [inpName, attrs] of data.entries) {
+          const inp = screen.getByLabelText(attrs.label) as HTMLInputElement;
+          expect(inp).toBeInTheDocument();
+          expect(inp.name).toBe(inpName);
+          expect(inp.defaultValue).toBe(attrs.defaultValue);
+        }
       });
 
       it('should not submit with empty fields', async () => {
-        await assertNotSubmitWithEmptyFields(formType);
+        const { data, user } = await setup(props);
+        for (const entry of data.entries) {
+          const inp = screen.getByLabelText(entry[1].label) as HTMLInputElement;
+          expect(inp.ariaInvalid).toBe('false');
+        }
+        const submitter = screen.getByRole('button', data.submitterOpts);
+        await user.click(submitter);
+        for (const entry of data.entries) {
+          const inp = screen.getByLabelText(entry[1].label) as HTMLInputElement;
+          expect(inp.ariaInvalid).toBe('true');
+        }
       });
     });
   }
 
+  const updateUserFormProps: AuthFormProps = {
+    formLabelId: 'test-update-user-form',
+    formType: 'signup',
+    user: author,
+  };
+
   it('should display the given user data in the form', async () => {
-    const { data } = await setup('signup', true);
+    const { data } = await setup(updateUserFormProps);
     for (const entry of data.entries) {
       const inp = screen.getByLabelText(entry[1].label) as HTMLInputElement;
       expect(inp.value).toStrictEqual(entry[1].defaultValue);
@@ -92,7 +87,7 @@ describe(`<AuthForm />`, () => {
   });
 
   it('should submit with empty fields if it is an update user form', async () => {
-    const { data, user } = await setup('signup', true);
+    const { data, user } = await setup(updateUserFormProps);
     for (const entry of data.entries) {
       if (['text', 'password', 'textarea'].includes(entry[1].type || '')) {
         user.clear(screen.getByLabelText(entry[1].label));
@@ -103,7 +98,7 @@ describe(`<AuthForm />`, () => {
   });
 
   it('should send `PUT` request on submit, and add the given-user id in the URL', async () => {
-    const { data, user } = await setup('signup', true);
+    const { data, user } = await setup(updateUserFormProps);
     await user.click(screen.getByRole('button', data.submitterOpts));
     await waitFor(() => expect(fetchSpy).toHaveBeenCalledOnce());
     const reqInit = fetchSpy.mock.calls[0][1] as RequestInit;
