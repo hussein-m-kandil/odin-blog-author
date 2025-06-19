c1cd0a144b45a793139e05964282c94630c325b6
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-19 11:02:06 +0300

    return mocks from `mockDialogContext` test utility

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -4,11 +4,16 @@ import { vi } from 'vitest';
 export const delay = (fn: () => void, ms = 100) => setTimeout(fn, ms);
 
 export const mockDialogContext = () => {
-  return vi.mock('@/contexts/dialog-context', () => {
-    return {
-      useDialog: vi.fn(() => ({ showDialog: vi.fn(), hideDialog: vi.fn() })),
-    };
+  const dialogMockedMethods = vi.hoisted(() => {
+    const showDialog = vi.fn();
+    const hideDialog = vi.fn();
+    const useDialog = vi.fn(() => ({ showDialog, hideDialog }));
+    return { useDialog, showDialog, hideDialog };
   });
+  vi.mock('@/contexts/dialog-context', () => ({
+    useDialog: dialogMockedMethods.useDialog,
+  }));
+  return dialogMockedMethods;
 };
 
 export const dates = {
