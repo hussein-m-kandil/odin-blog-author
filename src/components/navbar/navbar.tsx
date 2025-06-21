f37c6852c42512f22540252c12f419f6341a435b
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-21 17:27:29 +0300

    Make `Navbar` position `fixed` while scrolling up

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,48 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
 import { toast } from 'sonner';
 import { User } from '@/types';
 import { Button } from '@/components/ui/button';
-import { getUnknownErrorMessage } from '@/lib/utils';
 import { ModeToggle } from '@/components/mode-toggle';
 import { usePathname, useRouter } from 'next/navigation';
+import { cn, getUnknownErrorMessage } from '@/lib/utils';
 import { CreatePostDialog } from '@/components/create-post-dialog';
 
 export function Navbar({ user = null }: { user?: User | null }) {
+  const navContainerRef = React.useRef<HTMLDivElement>(null);
+
+  const [yScroll, setYScroll] = React.useState(0);
   const pathname = usePathname();
   const router = useRouter();
+
+  React.useEffect(() => {
+    const navContainer = navContainerRef.current;
+    if (navContainer && navContainer.firstElementChild) {
+      const nav = navContainer.firstElementChild;
+      const navHeight = nav.getBoundingClientRect().height;
+      const eventName = 'scroll';
+      const handleScroll = () => {
+        const currentYScroll = window.scrollY;
+        setYScroll(currentYScroll);
+        if (currentYScroll < yScroll) {
+          nav.classList.remove('-translate-y-100');
+          if (currentYScroll > navHeight * 0.75) {
+            navContainer.setAttribute('style', `padding-top: ${navHeight}px;`);
+            nav.classList.add('fixed');
+          } else {
+            navContainer.removeAttribute('style');
+            nav.classList.remove('fixed');
+          }
+        } else if (currentYScroll > navHeight) {
+          nav.classList.add('-translate-y-100');
+        }
+      };
+      window.addEventListener(eventName, handleScroll);
+      return () => window.removeEventListener(eventName, handleScroll);
+    }
+  }, [yScroll]);
 
   const handleSignout = async () => {
     const signoutUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/signout`;
@@ -37,30 +68,37 @@ export function Navbar({ user = null }: { user?: User | null }) {
   };
 
   return (
-    <nav className='flex flex-col sm:flex-row items-center justify-between gap-4 p-4 shadow-sm shadow-secondary'>
-      <div className='text-3xl font-normal'>
-        <Link href='/'>Odin Blog Author</Link>
-      </div>
-      <div className='flex items-center gap-2'>
-        {user ? (
-          <>
-            <CreatePostDialog />
-            <Button type='button' {...btnCommonProps} onClick={handleSignout}>
-              Sign out
+    <div ref={navContainerRef}>
+      <nav
+        className={cn(
+          'transition-transform duration-700 motion-reduce:transition-none motion-reduce:translate-y-0',
+          'flex flex-col sm:flex-row items-center justify-between gap-4 p-4 shadow-sm shadow-secondary',
+          'top-0 left-0 bottom-auto w-full bg-background/85 backdrop-blur-xs'
+        )}>
+        <div className='text-3xl font-normal'>
+          <Link href='/'>{process.env.NEXT_PUBLIC_APP_NAME}</Link>
+        </div>
+        <div className='flex items-center gap-2'>
+          {user ? (
+            <>
+              <CreatePostDialog />
+              <Button type='button' {...btnCommonProps} onClick={handleSignout}>
+                Sign out
+              </Button>
+            </>
+          ) : (
+            <Button type='button' {...btnCommonProps} asChild>
+              {pathname === '/signin' ? (
+                <Link href='/signup'>Sign up</Link>
+              ) : (
+                <Link href='/signin'>Sign in</Link>
+              )}
             </Button>
-          </>
-        ) : (
-          <Button type='button' {...btnCommonProps} asChild>
-            {pathname === '/signin' ? (
-              <Link href='/signup'>Sign up</Link>
-            ) : (
-              <Link href='/signin'>Sign in</Link>
-            )}
-          </Button>
-        )}
-        <ModeToggle />
-      </div>
-    </nav>
+          )}
+          <ModeToggle />
+        </div>
+      </nav>
+    </div>
   );
 }
 
