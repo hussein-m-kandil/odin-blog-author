28950c30c2c9a8c92fa6bbcf7ff9b40d364f0df1
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-06-22 11:36:31 +0300

    Accept props for customizing `ModeToggle`

--- src/components/mode-toggle/mode-toggle.tsx
+++ src/components/mode-toggle/mode-toggle.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-// https://ui.shadcn.com/docs/dark-mode/next#add-a-mode-toggle
-
 import React from 'react';
 import {
   DropdownMenu,
@@ -9,23 +7,36 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { useTheme } from 'next-themes';
-import { Moon, Sun } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { cn } from '@/lib/utils';
 
-export function ModeToggle() {
+export function ModeToggle({
+  triggerProps,
+  menuProps,
+}: {
+  menuProps?: React.ComponentProps<typeof DropdownMenuContent>;
+  triggerProps?: React.ComponentProps<typeof Button>;
+}) {
   const { setTheme } = useTheme();
+
+  const triggerCN = triggerProps?.className;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon' className='font-bold'>
+        <Button
+          size='icon'
+          variant='outline'
+          {...triggerProps}
+          className={cn('font-bold', triggerCN)}>
           <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent align='end' {...menuProps}>
         <DropdownMenuItem onClick={() => setTheme('light')}>
           Light
         </DropdownMenuItem>
