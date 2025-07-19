639a32867d7a34ff4c63be5fd6443a7063022190
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-07-19 19:46:03 +0300

    Refactor mocks in test setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -3,30 +3,45 @@ import {
   setupIntersectionMocking,
   resetIntersectionMocking,
 } from 'react-intersection-observer/test-utils';
-import { vi, beforeEach, beforeAll, afterEach } from 'vitest';
+import { vi, beforeAll, beforeEach, afterEach } from 'vitest';
+
+const nextRouterMock = vi.hoisted(() => ({
+  push: vi.fn(),
+  replace: vi.fn(),
+  prefetch: vi.fn(),
+  back: vi.fn(),
+}));
+
+vi.mock('next/navigation', async (importOriginal) => ({
+  ...(await importOriginal<typeof import('next/navigation')>()),
+  useRouter: () => nextRouterMock,
+}));
+
+const observeMock = vi.fn();
+const unobserveMock = vi.fn();
+const disconnectMock = vi.fn();
+
+beforeAll(() => {
+  global.ResizeObserver = class ResizeObserver {
+    constructor() {
+      this.observe = this.observe.bind(this);
+      this.unobserve = this.unobserve.bind(this);
+      this.disconnect = this.disconnect.bind(this);
+    }
+    observe = observeMock;
+    unobserve = unobserveMock;
+    disconnect = disconnectMock;
+  };
+});
 
 beforeEach(() => {
   setupIntersectionMocking(vi.fn);
 });
 
 afterEach(() => {
+  Object.values(nextRouterMock).forEach((mockFn) => mockFn.mockReset());
+  disconnectMock.mockReset();
+  unobserveMock.mockReset();
+  observeMock.mockReset();
   resetIntersectionMocking();
-});
-
-vi.mock('next/navigation', async (importOriginal) => ({
-  ...(await importOriginal<typeof import('next/navigation')>()),
-  useRouter: () => ({
-    push: vi.fn(),
-    replace: vi.fn(),
-    prefetch: vi.fn(),
-    back: vi.fn(),
-  }),
-}));
-
-beforeAll(() => {
-  global.ResizeObserver = class ResizeObserver {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
-  };
 });
