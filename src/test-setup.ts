f13ebd043736f28f64481d46d9b86aa43a672401
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-07-19 19:30:29 +0300

    Mock `IntersectionObserver` API in test setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,6 +1,17 @@
 import '@testing-library/jest-dom/vitest';
-import { vi } from 'vitest';
-import { beforeAll } from 'vitest';
+import {
+  setupIntersectionMocking,
+  resetIntersectionMocking,
+} from 'react-intersection-observer/test-utils';
+import { vi, beforeEach, beforeAll, afterEach } from 'vitest';
+
+beforeEach(() => {
+  setupIntersectionMocking(vi.fn);
+});
+
+afterEach(() => {
+  resetIntersectionMocking();
+});
 
 vi.mock('next/navigation', async (importOriginal) => ({
   ...(await importOriginal<typeof import('next/navigation')>()),
