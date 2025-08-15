384153dbe8294d46a4d06f612a3f7382d0ac8687
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-08-15 14:34:15 +0300

    Control dialog hiding via callback given on show

--- src/contexts/dialog-context/dialog-context.test.tsx
+++ src/contexts/dialog-context/dialog-context.test.tsx
@@ -1,9 +1,12 @@
 import { DialogData, DialogProvider, useDialog } from './dialog-context';
 import { userEvent } from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+const shouldHideMock = vi.fn<() => boolean | Promise<boolean>>(() => true);
 
 let dialogData: DialogData;
+
 function DialogConsumer() {
   const { showDialog, hideDialog } = useDialog();
   dialogData = {
@@ -17,55 +20,98 @@ function DialogConsumer() {
     ),
   };
   return (
-    <button type='button' onClick={() => showDialog(dialogData)}>
-      Show Dialog
-    </button>
+    <DialogProvider>
+      <div>
+        <button
+          type='button'
+          onClick={() => showDialog(dialogData, shouldHideMock)}>
+          Show Dialog
+        </button>
+      </div>
+    </DialogProvider>
   );
 }
 
+const DialogConsumerWrapper = () => {
+  return (
+    <DialogProvider>
+      <div>
+        <DialogConsumer />
+      </div>
+    </DialogProvider>
+  );
+};
+
+const assertDialogHidden = () => {
+  expect(screen.queryByRole('button', { name: /hide/i })).toBeNull();
+  expect(screen.queryByText(dialogData.title as string)).toBeNull();
+  expect(screen.queryByText(dialogData.footer as string)).toBeNull();
+  expect(screen.queryByText(dialogData.description as string)).toBeNull();
+};
+
+const assertDialogVisible = () => {
+  expect(screen.getByRole('button', { name: /hide/i })).toBeInTheDocument();
+  expect(screen.getByText(dialogData.title as string)).toBeInTheDocument();
+  expect(screen.getByText(dialogData.footer as string)).toBeInTheDocument();
+  expect(
+    screen.getByText(dialogData.description as string)
+  ).toBeInTheDocument();
+};
+
+afterEach(vi.clearAllMocks);
+
 describe('DialogContext', () => {
   it('should `useDialog` throw if used outside of `DialogProvider`', () => {
-    expect(() =>
-      render(
-        <div>
-          <DialogConsumer />
-        </div>
-      )
-    ).toThrowError(/DialogProvider/i);
-    expect(() =>
-      render(
-        <DialogProvider>
-          <div>
-            <DialogConsumer />
-          </div>
-        </DialogProvider>
-      )
-    ).not.toThrowError();
+    expect(() => render(<DialogConsumer />)).toThrowError(/DialogProvider/i);
+    expect(() => render(<DialogConsumerWrapper />)).not.toThrowError();
   });
 
   it('should show/hide the given dialog data correctly', async () => {
     const user = userEvent.setup();
-    render(
-      <DialogProvider>
-        <div>
-          <DialogConsumer />
-        </div>
-      </DialogProvider>
-    );
-    expect(screen.queryByRole('button', { name: /hide/i })).toBeNull();
-    expect(screen.queryByText(dialogData.title as string)).toBeNull();
-    expect(screen.queryByText(dialogData.footer as string)).toBeNull();
-    expect(screen.queryByText(dialogData.description as string)).toBeNull();
+    render(<DialogConsumerWrapper />);
+    assertDialogHidden();
     await user.click(screen.getByRole('button', { name: /show/i }));
-    expect(screen.getByText(dialogData.title as string)).toBeInTheDocument();
-    expect(screen.getByText(dialogData.footer as string)).toBeInTheDocument();
-    expect(
-      screen.getByText(dialogData.description as string)
-    ).toBeInTheDocument();
+    assertDialogVisible();
     await user.click(screen.getByRole('button', { name: /hide/i }));
-    expect(screen.queryByRole('button', { name: /hide/i })).toBeNull();
-    expect(screen.queryByText(dialogData.title as string)).toBeNull();
-    expect(screen.queryByText(dialogData.footer as string)).toBeNull();
-    expect(screen.queryByText(dialogData.description as string)).toBeNull();
+    assertDialogHidden();
+  });
+
+  it('should close after clicking the close button', async () => {
+    const user = userEvent.setup();
+    render(<DialogConsumerWrapper />);
+    await user.click(screen.getByRole('button', { name: /show/i }));
+    assertDialogVisible();
+    await user.click(screen.getByRole('button', { name: /close/i }));
+    assertDialogHidden();
+  });
+
+  it('should not close after clicking the close button', async () => {
+    shouldHideMock.mockImplementationOnce(() => false);
+    const user = userEvent.setup();
+    render(<DialogConsumerWrapper />);
+    await user.click(screen.getByRole('button', { name: /show/i }));
+    assertDialogVisible();
+    await user.click(screen.getByRole('button', { name: /close/i }));
+    assertDialogVisible();
+  });
+
+  it('should close after clicking the close button, and awaiting `shouldHide`', async () => {
+    shouldHideMock.mockImplementationOnce(() => Promise.resolve(true));
+    const user = userEvent.setup();
+    render(<DialogConsumerWrapper />);
+    await user.click(screen.getByRole('button', { name: /show/i }));
+    assertDialogVisible();
+    await user.click(screen.getByRole('button', { name: /close/i }));
+    assertDialogHidden();
+  });
+
+  it('should not close after clicking the close button, and awaiting `shouldHide`', async () => {
+    shouldHideMock.mockImplementationOnce(() => Promise.resolve(false));
+    const user = userEvent.setup();
+    render(<DialogConsumerWrapper />);
+    await user.click(screen.getByRole('button', { name: /show/i }));
+    assertDialogVisible();
+    await user.click(screen.getByRole('button', { name: /close/i }));
+    assertDialogVisible();
   });
 });
--- src/contexts/dialog-context/dialog-context.tsx
+++ src/contexts/dialog-context/dialog-context.tsx
@@ -17,8 +17,13 @@ export interface DialogData {
   description: React.ReactNode;
 }
 
+const initShouldHideFn: () => Promise<boolean> | boolean = () => true;
+
 export interface DialogContextValue {
-  showDialog: (data: DialogData) => void;
+  showDialog: (
+    data: DialogData,
+    shouldHideDialog?: typeof initShouldHideFn
+  ) => void;
   hideDialog: () => void;
 }
 
@@ -36,24 +41,32 @@ export function DialogProvider({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // React interpret function state-value as state initializer/updater
+  // So, initialize/update function state-value via a wrapper function
+  const [shouldHide, setShouldHide] = React.useState(() => initShouldHideFn);
   const [dialogData, setDialogData] = React.useState<DialogData>(initData);
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
 
-  const showDialog = (data: DialogData) => {
-    setDialogData(data);
-    setIsDialogOpen(true);
+  const contextValue: DialogContextValue = {
+    showDialog: (data: DialogData, shouldHideDialog) => {
+      setShouldHide(() => shouldHideDialog || initShouldHideFn);
+      setIsDialogOpen(true);
+      setDialogData(data);
+    },
+    hideDialog: () => setIsDialogOpen(false),
   };
 
-  const hideDialog = () => {
-    setIsDialogOpen(false);
+  const handleOpenChange = async (open: boolean) => {
+    const hide = await shouldHide();
+    setIsDialogOpen(open === false ? !hide : open);
   };
 
   return (
-    <DialogContext value={{ showDialog, hideDialog }}>
+    <DialogContext value={contextValue}>
       {children}
       <Dialog
         open={isDialogOpen}
-        onOpenChange={setIsDialogOpen}
+        onOpenChange={handleOpenChange}
         key={Date.now()}>
         <DialogContent className='sm:max-w-lg max-h-[calc(100vh-2rem)] overflow-auto'>
           <DialogHeader>
