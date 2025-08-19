085db3d89a896bad2d8b47acc527c125ce8fc86a
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-08-19 10:17:19 +0300

    Apply `ImageForm` changes only on explicit submit

--- src/components/image-form/image-form.tsx
+++ src/components/image-form/image-form.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import React, { ChangeEvent } from 'react';
-import { cn, parseAxiosAPIError, getUnknownErrorMessage } from '@/lib/utils';
+import React from 'react';
+import {
+  cn,
+  parseAxiosAPIError,
+  getUnknownErrorMessage,
+  getNewImageDataFromImage,
+} from '@/lib/utils';
 import { MutableImage, MutableImageSkeleton } from '@/components/mutable-image';
-import { Image as ImageType, NewImage } from '@/types';
+import { Loader, ImageUp, ImagePlus, ImageMinus } from 'lucide-react';
+import { Image, Image as ImageType, NewImage } from '@/types';
 import { useAuthData } from '@/contexts/auth-context';
 import { ImageFormProps } from './image-form.types';
 import { Progress } from '@/components/ui/progress';
 import { Button } from '@/components/ui/button';
-import { Loader, Upload } from 'lucide-react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { AxiosResponse } from 'axios';
 import { toast } from 'sonner';
 
 export function ImageForm({
@@ -23,7 +27,10 @@ export function ImageForm({
   image,
   ...props
 }: ImageFormProps) {
-  const [newImage, setNewImage] = React.useState<NewImage | null>(null);
+  const [newImageData, setImageData] = React.useState<NewImage | null>(
+    image ? getNewImageDataFromImage(image) : null
+  );
+  const [imageHasUpdates, setImageHasUpdates] = React.useState(false);
   const [uploadPercent, setUploadPercent] = React.useState(-1);
   const [uploading, setUploading] = React.useState(false);
   const [file, setFile] = React.useState<File | null>();
@@ -34,138 +41,163 @@ export function ImageForm({
 
   const fileInpRef = React.useRef<HTMLInputElement>(null);
 
+  const isUpdate = !!image;
+  const endpoint = '/images';
+  const imageShouldBeDeleted = isUpdate && !newImageData;
+
   React.useImperativeHandle(uploadingRef, () => uploading, [uploading]);
 
   const setNewImageData = (selectedFile: File) => {
     setFile(selectedFile);
-    if (newImage) URL.revokeObjectURL(newImage.src);
+    if (newImageData) URL.revokeObjectURL(newImageData.src);
     const src = URL.createObjectURL(selectedFile);
-    setNewImage({ yPos: 50, xPos: 50, info: '', alt: '', src });
+    setImageData({ yPos: 50, xPos: 50, info: '', alt: '', src });
   };
 
   const resetNewImageData = () => {
     if (fileInpRef.current) fileInpRef.current.value = '';
-    if (newImage) URL.revokeObjectURL(newImage.src);
-    setNewImage(null);
+    if (newImageData) URL.revokeObjectURL(newImageData.src);
+    setImageData(null);
     setFile(null);
   };
 
-  const endpoint = '/images';
-  const updating = !!image;
+  React.useEffect(() => {
+    return () => {
+      if (file && newImageData) {
+        URL.revokeObjectURL(newImageData.src);
+      }
+    };
+  }, [file, newImageData]);
 
-  let url: string, method: 'post' | 'put', verb: 'Update' | 'Upload';
-  if (updating) {
-    url = `${endpoint}/${image.id}`;
-    verb = 'Update';
-    method = 'put';
-  } else {
-    url = endpoint;
-    verb = 'Upload';
-    method = 'post';
-  }
+  const submitUpdate = async ({ id, ...oldData }: Image, newData: NewImage) => {
+    let result = null;
+    try {
+      setUploading(true);
+      const url = `${endpoint}/${id}`;
+      const body = { ...oldData, ...newData };
+      const { data } = await authAxios.put<ImageType>(url, body);
+      setImageHasUpdates(false);
+      onSuccess?.(data);
+      toast.success('Update succeeded', {
+        description: 'Your image have been updated successfully',
+      });
+      result = data;
+    } catch (error) {
+      const { message } = parseAxiosAPIError(error);
+      if (!message) onFailed?.(error);
+      toast.error('Update failed', {
+        description: message || getUnknownErrorMessage(error),
+      });
+    } finally {
+      setUploading(false);
+    }
+    return result;
+  };
+
+  const submitDelete = async (id: Image['id']) => {
+    let result: null | undefined;
+    try {
+      setUploading(true);
+      const { data } = await authAxios.delete<null>(`${endpoint}/${id}`);
+      onSuccess?.(data);
+      toast.success('Delete succeeded', {
+        description: 'Your image have been deleted successfully',
+      });
+      result = null;
+    } catch (error) {
+      onFailed?.(error);
+      toast.error('Delete failed', {
+        description:
+          parseAxiosAPIError(error).message || getUnknownErrorMessage(error),
+      });
+    } finally {
+      setUploading(false);
+    }
+    return result;
+  };
+
+  const submitImageFile = async (imageFile: File, e: React.SyntheticEvent) => {
+    let result = null;
+    try {
+      setUploading(true);
+      const body = new FormData();
+      body.set('image', imageFile);
+      if (newImageData) {
+        const newImageEntries = Object.entries(newImageData);
+        for (const [k, v] of newImageEntries) {
+          body.set(k, v);
+        }
+        body.delete('src');
+      }
+      let url: string, method: 'post' | 'put';
+      if (isUpdate) {
+        url = `${endpoint}/${image.id}`;
+        method = 'put';
+      } else {
+        url = endpoint;
+        method = 'post';
+      }
+      const { data } = await authAxios<ImageType>({
+        url,
+        method,
+        data: body,
+        onUploadProgress: ({ loaded, total }) => {
+          if (loaded && total) {
+            const intProgressPercent = Math.floor((loaded / total) * 100);
+            const newUploadPercent = (intProgressPercent % 100) - 1;
+            setUploadPercent(newUploadPercent);
+          }
+        },
+      });
+      resetNewImageData();
+      (e.target as HTMLFormElement).reset();
+      toast.success('Upload succeeded', {
+        description: 'Your image have been uploaded successfully',
+      });
+      onSuccess?.(data);
+      result = data;
+    } catch (error) {
+      const { message: description } = parseAxiosAPIError(error);
+      toast.error('Upload failed', { description });
+      onFailed?.(error);
+    } finally {
+      setUploading(false);
+    }
+    return result;
+  };
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
+    let result: Image | null = null;
     if (file) {
-      try {
-        setUploading(true);
-        const body = new FormData();
-        body.set('image', file);
-        if (newImage) {
-          const newImageEntries = Object.entries(newImage);
-          for (const [k, v] of newImageEntries) {
-            body.set(k, v);
-          }
-          body.delete('src');
-        }
-        const { data } = await authAxios[method]<ImageType>(url, body, {
-          onUploadProgress: ({ loaded, total }) => {
-            if (loaded && total) {
-              const intProgressPercent = Math.floor((loaded / total) * 100);
-              const newUploadPercent = (intProgressPercent % 100) - 1;
-              setUploadPercent(newUploadPercent);
-            }
-          },
-        });
-        resetNewImageData();
-        (e.target as HTMLFormElement).reset();
-        toast.success('Upload succeeded', {
-          description: 'Your image have been uploaded successfully',
-        });
-        onSuccess?.(data);
-      } catch (error) {
-        const { message: description } = parseAxiosAPIError(error);
-        toast.error('Upload failed', { description });
-        onFailed?.(error);
-      } finally {
-        setUploading(false);
+      result = await submitImageFile(file, e);
+    } else if (isUpdate) {
+      if (imageShouldBeDeleted) {
+        const deleteRes = await submitDelete(image.id);
+        if (deleteRes !== undefined) result = deleteRes;
+      } else if (imageHasUpdates && newImageData) {
+        result = await submitUpdate(image, newImageData);
+      } else {
+        toast.warning('There are no pending changes to submit!');
       }
     } else {
-      const description = 'Choose an image to upload';
-      toast.error('Image file is missing', { description });
+      toast.error('Image file is missing');
     }
+    setImageData(result ? getNewImageDataFromImage(result) : null);
   };
 
-  const updateImageData = async ({ id, ...imageData }: NewImage) => {
-    if (id) {
-      toast.promise<AxiosResponse>(
-        authAxios.put<ImageType>(`${endpoint}/${id}`, imageData),
-        {
-          loading: 'Updating image data...',
-          success: async ({ data: updatedImage }) => {
-            onSuccess?.(updatedImage);
-            return {
-              message: 'Update succeeded',
-              description: 'Your image have been updated successfully',
-            };
-          },
-          error: (error) => {
-            const { message: description } = parseAxiosAPIError(error);
-            if (description) return { message: 'Update failed', description };
-            onFailed?.(error);
-            return {
-              description: getUnknownErrorMessage(error),
-              message: 'Update failed',
-            };
-          },
-        }
-      );
-    } else {
-      setNewImage(imageData);
-    }
+  const updateImage = async (data: NewImage) => {
+    setImageData(data);
+    setImageHasUpdates(isUpdate);
   };
 
-  const deleteImage = async ({ id }: NewImage) => {
-    if (id) {
-      toast.promise<AxiosResponse>(
-        authAxios.delete<null>(`${endpoint}/${id}`),
-        {
-          loading: 'Deleting image...',
-          success: async ({ data }) => {
-            onSuccess?.(data);
-            return {
-              message: 'Delete succeeded',
-              description: 'Your image have been deleted successfully',
-            };
-          },
-          error: (error) => {
-            const { message } = parseAxiosAPIError(error);
-            onFailed?.(error);
-            return {
-              message: 'Delete failed',
-              description: message || getUnknownErrorMessage(error),
-            };
-          },
-        }
-      );
-    } else {
-      resetNewImageData();
-    }
+  const deleteImage = async () => {
+    resetNewImageData();
   };
 
-  const handleFileChange: React.EventHandler<ChangeEvent<HTMLInputElement>> = (
-    e
-  ) => {
+  const handleFileChange: React.EventHandler<
+    React.ChangeEvent<HTMLInputElement>
+  > = (e) => {
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
       if (selectedFile.type.startsWith('image/')) {
@@ -177,11 +209,26 @@ export function ImageForm({
     }
   };
 
+  const submitter = imageShouldBeDeleted
+    ? {
+        idle: { icon: <ImageMinus />, label: `Delete ${label.toLowerCase()}` },
+        submitting: { label: 'Deleting...' },
+      }
+    : isUpdate && !file
+    ? {
+        idle: { icon: <ImagePlus />, label: `Update ${label.toLowerCase()}` },
+        submitting: { label: 'Updating...' },
+      }
+    : {
+        idle: { icon: <ImageUp />, label: `Upload ${label.toLowerCase()}` },
+        submitting: { label: 'Uploading...' },
+      };
+
   return (
     <form
       {...props}
       onSubmit={handleSubmit}
-      aria-label={`${verb} image`}
+      aria-label={submitter.idle.label}
       className={cn('w-full my-4 space-y-2', className)}>
       <Label htmlFor='image'>{label}</Label>
       <div className='relative'>
@@ -189,8 +236,8 @@ export function ImageForm({
           <MutableImageSkeleton />
         ) : (
           <MutableImage
-            image={newImage || image}
-            mutation={{ update: updateImageData, delete: deleteImage }}
+            image={newImageData}
+            mutation={{ update: updateImage, delete: deleteImage }}
           />
         )}
         {uploadPercent >= 0 && (
@@ -209,18 +256,22 @@ export function ImageForm({
         disabled={uploading}
         onChange={handleFileChange}
       />
-      <Button type='submit' className='w-full' disabled={!file || uploading}>
+      <Button
+        type='submit'
+        className='w-full'
+        disabled={
+          (!file && !imageHasUpdates && !imageShouldBeDeleted) || uploading
+        }>
         {uploading ? (
-          <Loader
-            aria-label='Uploading'
-            className='inline-block animate-spin'
-          />
+          <>
+            <Loader aria-label='Uploading' className='animate-spin' />{' '}
+            {submitter.submitting.label}
+          </>
         ) : (
-          <Upload className='inline-block' />
+          <>
+            {submitter.idle.icon} {submitter.idle.label}
+          </>
         )}
-        <span>
-          {verb} {label.toLowerCase()}
-        </span>
       </Button>
     </form>
   );
