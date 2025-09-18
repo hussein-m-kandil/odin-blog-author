ae55d5f92f6cb3dbfe523c14da32c2bd304bee66
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-09-18 18:09:15 +0300

    Inform the users about the app kind and purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import {
   HydrationBoundary,
 } from '@tanstack/react-query';
 import { PostsWrapper, PostsWrapperSkeleton } from '@/components/posts-wrapper';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { AppStats, AppStatsSkeleton } from '@/components/app-stats';
 import { getServerAuthData } from '@/lib/auth';
 import { H1 } from '@/components/typography/';
 import { Header } from '@/components/header';
 import { SearchParams } from '@/types';
+import { Bell } from 'lucide-react';
 
 export default async function Home({
   searchParams,
@@ -29,6 +31,28 @@ export default async function Home({
     <>
       <Header>
         <H1>{process.env.NEXT_PUBLIC_APP_NAME || 'Home Page'}</H1>
+        <Alert className='w-fit max-w-2xl mx-auto my-4 text-start'>
+          <Bell />
+          <AlertTitle>This is a Demo Website!</AlertTitle>
+          <AlertDescription className='*:leading-snug!'>
+            <p>
+              I built this project to showcase what I am learning in web
+              development, and I do not plan to keep maintaining it; therefore,
+              I added a reset feature to periodically delete any non-admin data.
+              See the{' '}
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://github.com/hussein-m-kandil/odin-blog-author'
+                className={`font-medium decoration-muted-foreground/75
+                  underline underline-offset-2 decoration-0 decoration-dotted
+                  hover:decoration-muted-foreground active:text-foreground`}>
+                GitHub repository
+              </a>{' '}
+              for more information.
+            </p>
+          </AlertDescription>
+        </Alert>
       </Header>
       <main>
         <React.Suspense fallback={<AppStatsSkeleton />}>
